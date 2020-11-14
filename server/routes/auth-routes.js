2995e3a10eedefb6e6fa9c76f2ae312dee22f4d4
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-14 21:15:34 +0100

    first commit

---
+++ server/routes/auth-routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+})
+
+
+router.post('/login', (req, res) => {
+    console.log(req.body);
+})
+
+
+router.get('/user', (req, res) => {
+    console.log(req.body);
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,21 +10,35 @@ const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 
+const authRoutes = require("./routes/auth-routes");
+
 const app = express();
 
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(port, () => {
-    console.log(`Listening on ${port} `)
-});
-
 app.use(
     cors({
       credentials: true,
-      origin: ["http://localhost:3000"] // <== this will be the URL of our React app (it will be running on port 3000)
+      origin: ["http://localhost:3000"] // <== URL of React app (it will be running on port 3000)
     })
   );
+
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true    
+}));
+
+app.use(cookieParser("secretcode"));
+
+// Routes
+app.use("/auth", authRoutes);
+
+  app.listen(port, () => {
+      console.log(`Listening on ${port} `)
+  });
 
   module.exports = app;
