0f2a34b3f5992341e311b265ce4d353d6c447e5a
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-15 17:05:18 +0100

    started refactor of App.js by extracting code to login page, added open issues to README

---
+++ README.md
@@ -0,0 +1,9 @@
+Current blockers:
+axios / cors issue for post request on signup
+
+Quick fixes:
+how do I set auto-formatting (e.g. with indentation on save)? Prettier already installed.
+
+Questions:
+can you use hooks in React Native?
+can / should I use buttons for navigation? clickthrough. Or should this be all links?
--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,15 @@
 
-import React, { useState } from "react";
+import React from "react";
 import './App.css';
-import axios from "axios";
 import LandingPage from "./Views/LandingPage";
+import LoginPage from "./Views/Auth/LoginPage";
 
 
 function App() {
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginUsername, setLoginUsername] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  const register = () => {
-    axios({
-      method: "post",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-      },
-      withCredentials: true,
-      url: "http://localhost/5000/register", //so long as this is being run locally | GETTING CONNECTION ERROR
-    }).then((res) => console.log(res))
-    .catch((err) => console.log(err));
-  };
-  const login = () => {};
-  const getUser = () => {};
-
   return (
     <div className="App">
-
-
     <LandingPage />
-    <div>
-      <h1>Register</h1>
-      <input placeholder="username" onChange={e => setRegisterUsername(e.target.value)} />  
-      <input placeholder="password" onChange={e => setRegisterPassword(e.target.value)} />
-      <button onClick={register}>Submit</button>
-    </div>
-
-
-    <div>
-      <h1>Login</h1>
-      <input placeholder="username" onChange={e => setLoginUsername(e.target.value)} />  
-      <input placeholder="password" onChange={e => setLoginPassword(e.target.value)}/>
-      <button onClick={login}>Submit</button>
-    </div>
-
-
-    <div>
-      <h1>Get User</h1>
-      <button onClick={getUser}>Submit</button>
-    </div>
-
+    <LoginPage />
     </div>
   );
 }
