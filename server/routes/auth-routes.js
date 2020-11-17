c64669fa5830b21327d8491438cf9cd6d28765e0
Yelizaveta Crofts <yscrofts@hotmail.com>
2020-11-17 13:49:03 +0100

    updated /register route to post user to db

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -3,16 +3,29 @@ const User = require("../schemas/user");
 
 router.post('/register', (req, res) => {
     console.log(req.body);
-})
+
+    User.findOne({username: req.body.username}, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User Already Exists");
+        if (!doc) {
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.password
+            });
+            await newUser.save()
+            res.send("New user created");
+        }
+    })
+});
 
 
 router.post('/login', (req, res) => {
     console.log(req.body);
-})
+});
 
 
 router.get('/user', (req, res) => {
     console.log(req.body);
-})
+});
 
 module.exports = router;
